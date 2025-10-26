8e45fef810f6a4678133017fb89eae928b1a5359
cyril <cyril.d@wanadoo.fr>
2025-10-26 14:51:00 +0100

    add real table

--- app.py
+++ app.py
@@ -1,15 +1,49 @@
 import streamlit as st
 import pandas as pd
 import duckdb
+import io
 
-data = {
-    "a": [1, 2, 3],
-    "b": [4, 5, 6]
-}
-df = pd.DataFrame(data)
+csv = '''
+beverage, price
+orange juice, 2.5
+Expresso, 2
+Tea,3
+'''
+beverages = pd.read_csv(io.StringIO(csv))
 
-st.write(df)
+csv2 = '''
+food_item, food_price
+cookie juice, 2.5
+chocolatine, 2
+muffin, 3
+'''
+food_items = pd.read_csv(io.StringIO(csv2))
 
-sql_query = st.text_area("Input sql query to extract from df:", value="")
-q_sql = duckdb.query(f"{sql_query}").df()
-st.write(q_sql)
+answer = '''
+SELECT * FROM beverages
+CROSS JOIN food_items
+'''
+
+solution = duckdb.sql(answer).df()
+
+st.header("Enter your SQL query:")
+sql_query = st.text_area(label="Input sql query...", key="user_input")
+
+if sql_query:
+    result = duckdb.sql(sql_query).df()
+    st.dataframe(result)
+
+
+
+tab1, tab2 = st.tabs(["Tables", "Solution"])
+
+with tab1:
+    st.write("table: beverages")
+    st.dataframe(beverages)
+    st.write("table: food_items")
+    st.dataframe(food_items)
+    st.write("expected:")
+    st.dataframe(solution)
+
+with tab2:
+    st.write(answer)
